1fbc9467067a5ecb81b7c1b37035e2317f818272
Chris Rosenau <chris@magentowizard.com>
2023-03-30 19:59:47 -0600

    Update fix_product_images.php
    Clean up code

--- fix_product_images.php
+++ fix_product_images.php
@@ -1,10 +1,11 @@
 <?php
-// Fixes products images assigned to a store that the product isn't assigned to
-// Author Chris Rosenau
-
-include '../app/bootstrap.php';
+// Fixes product images assigned to a store that the product isn't assigned to
+// Author: Chris Rosenau
 
 use Magento\Framework\App\Bootstrap;
+use Magento\Framework\App\ResourceConnection;
+
+require '../app/bootstrap.php';
 
 $bootstrap = Bootstrap::create(BP, $_SERVER);
 $objectManager = $bootstrap->getObjectManager();
@@ -12,57 +13,58 @@ $objectManager = $bootstrap->getObjectManager();
 $state = $objectManager->get('Magento\Framework\App\State');
 $state->setAreaCode('frontend');
 
-$resource = $objectManager->get('Magento\Framework\App\ResourceConnection');
+$resource = $objectManager->get(ResourceConnection::class);
 $connection = $resource->getConnection();
 
+// Attribute ids for images
 // Change these if your attributes ids are different
-// 87 -  imabe/base
+// 87 -  image/base
 // 88 -  small_image
 // 89 -  thumbnail
 // 134 - swatch_image
 // 144 - alt_image
-// 181 - series  
-$sql = "SELECT * FROM catalog_product_entity_varchar WHERE attribute_id = 87 OR attribute_id = 88 OR attribute_id = 89 OR attribute_id = 134 OR attribute_id = 144 OR attribute_id = 181";
-$allProductImages = $connection->fetchAll($sql);
+// 181 - series
+$imageAttributeIds = [87, 88, 89, 134, 144, 181];
 
+// Get all product images
+$sql = "SELECT * FROM catalog_product_entity_varchar WHERE attribute_id IN (" . implode(',', $imageAttributeIds) . ")";
+$productImages = $connection->fetchAll($sql);
+
+// Get all products
 $sql = "SELECT entity_id, sku FROM catalog_product_entity";
-$allProducts = $connection->fetchAll($sql);
+$products = $connection->fetchAll($sql);
 
-$remove = array();
+$removeValueIds = [];
 
-foreach ($allProducts as $product) {
+foreach ($products as $product) {
+    $productId = $product['entity_id'];
 
-    $entity_id = $product['entity_id'];
+    // Get all product stores
+    $sql = "SELECT a.entity_id as entity_id, a.sku as sku, b.store_id as store_id  FROM catalog_product_entity as a, catalog_product_entity_varchar as b WHERE a.entity_id = ? and a.entity_id = b.entity_id and b.attribute_id = 73";
+    $productStores = $connection->fetchAll($sql, [$productId]);
 
-    // get all product stores
-    $sql = "SELECT a.entity_id as entity_id, a.sku as sku, b.store_id as store_id  FROM catalog_product_entity as a, catalog_product_entity_varchar as b WHERE a.entity_id = $entity_id and a.entity_id = b.entity_id and b.attribute_id = 73";
-    $allProductStores = $connection->fetchAll($sql);
-
-    // search all images
-    
-    foreach ($allProductImages as $image) {
+    // Search all images
+    foreach ($productImages as $image) {
+        if ($image['entity_id'] != $productId) {
+            continue;
+        }
         $foundStore = false;
-        if ($image['entity_id'] == $entity_id){
-            foreach ($allProductStores as $imageStore) {
-                if ($image['store_id'] == $imageStore['store_id']){
-                    $foundStore = true;
-                }
+        foreach ($productStores as $store) {
+            if ($image['store_id'] == $store['store_id']) {
+                $foundStore = true;
+                break;
             }
-            if ($foundStore == false){
-                $remove[] = $image['value_id'];
-            }
-        }   
+        }
+        if (!$foundStore) {
+            $removeValueIds[] = $image['value_id'];
+        }
     }
-
 }
 
-// remove images
-foreach ($remove as $value) {
-    $sql = "DELETE FROM catalog_product_entity_varchar WHERE value_id = $value";
+// Remove images
+if (!empty($removeValueIds)) {
+    $sql = "DELETE FROM catalog_product_entity_varchar WHERE value_id IN (" . implode(',', $removeValueIds) . ")";
     $connection->query($sql);
 }
 
-
-
-echo "Finished cleaning product images
-";
+echo "Finished cleaning product images\n";
